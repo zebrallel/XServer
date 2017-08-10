5ff22a6342d37cffebd1b61fd5ccd28ace9cf0e0
xuejian.xu <jearbilove@yeah.net>
2017-08-10 22:52:50 +0800

    update

--- router/index.js
+++ router/index.js
@@ -8,7 +8,7 @@ var router = require('express').Router();
 
 router.all('*', function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Expose-Headers', 'content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
     next();
 });
