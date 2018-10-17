8104de1dc757e5415f347ae15a4c79e780ed5796
xuejian.xu <jearbilove@yeah.net>
2018-10-17 11:04:21 +0800

    update

--- router/api/index.js
+++ router/api/index.js
@@ -2,22 +2,33 @@
  * Created by xuejian.xu on 2017/8/10.
  */
 
-var router = require('express').Router();
+var router = require('express').Router()
 
 router.get('/csrf', function(req, res, next) {
     var options = {
         maxAge: 2592000000
-    };
+    }
 
-    res.cookie('username', 'xuxuejian', options);
+    res.cookie('username', 'xuxuejian', options)
 
     res.send('hello world')
-});
+})
 
-router.get('/show', function(req, res, next){
-    console.log('111');
+router.get('/show', function(req, res, next) {
+    console.log('111')
 
     res.end('success')
 })
 
-module.exports = router;
+router.get('/she', function(req, res) {
+    res.json([{ name: 'callie', age: 18 }, { name: 'LittleXu', age: 20 }])
+})
+
+router.post('/me', function(req, res) {
+    res.json({
+        name: 'xxj',
+        age: 20
+    })
+})
+
+module.exports = router
